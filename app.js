d1fc114988e11c64a3980f116a30b1a65dcb78e1
sigalor <juna99@gmx.de>
2017-11-18 01:07:46 +0100

    change app.js and package.json

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname + "/public")));
 
 app.listen(80, function() {
 	fs.writeFileSync("test.txt", "listening successful");
