1085067ab589d72258e9aebafc915bb7ec5f4c65
unknown <marvin.heinzelmann@t-online.de>
2017-11-19 02:16:38 +0100

    Put functionality of responseTime.js into a function that can be used from within other files via require.

--- responseTime.js
+++ responseTime.js
@@ -2,68 +2,68 @@ messages = require('./convert_raw_data/txtConverter')
 const CONVERSATION_START_THRESHOLD_HOURS = require('./customCalculations.js')
                                             .CONVERSATION_START_THRESHOLD_HOURS
 
-var convStartIndices = [0]
+exports = function(users) { 
+    var convStartIndices = [0]
 
-messages.forEach( (msg, i) => {
-    if (i > 0) {
-        var currentMillis = msg.timestamp
-        var lastMillis = messages[i-1].timestamp
+    messages.forEach( (msg, i) => {
+        if (i > 0) {
+            var currentMillis = msg.timestamp
+            var lastMillis = messages[i-1].timestamp
 
-        if ( (currentMillis - lastMillis) >=
-                1000 * 60 * 60 * CONVERSATION_START_THRESHOLD_HOURS) {
+            if ( (currentMillis - lastMillis) >=
+                    1000 * 60 * 60 * CONVERSATION_START_THRESHOLD_HOURS) {
+                
+                convStartIndices.push(i)
+            }
+        }
+    });
+
+    var result = []
+
+    users.forEach(user => {   //put a default user object into result for all users
+        result.push({
+            'name': user.name,
+            'responses': 0,
+            'notResponded': 0,
+            'started': 0,
+            'responseTimes': 0  // sum of all the response times (in seconds)
+        })
+    });
+
+    convStartIndices.forEach((startMsgIndex, convIndex) => {  // loop over conversations
+        var convStartMillis = messages[startMsgIndex].timestamp
+        var startUserName = messages[startMsgIndex].author
+
+        // loop over messages of the conversation
+        for (let i = startMsgIndex; i < convStartIndices[convIndex + 1] 
+                || convIndex + 1 == convStartIndices.length; i++) {
             
-            convStartIndices.push(i)
-        }
-    }
-});
-
-var users = [{'name': 'Sandesh'}, {'name': 'Marvin Heinzelmann'},
-            {'name': 'Daniel'}, {'name': 'Alina Weber'}]
-var result = []
-
-users.forEach(user => {   //put a default user object into result for all users
-    result.push({
-        'name': user.name,
-        'responses': 0,
-        'notResponded': 0,
-        'started': 0,
-        'responseTimes': 0  // sum of all the response times (in seconds)
-    })
-});
-
-convStartIndices.forEach((startMsgIndex, convIndex) => {  // loop over conversations
-    var convStartMillis = messages[startMsgIndex].timestamp
-    var startUserName = messages[startMsgIndex].author
-
-    // loop over messages of the conversation
-    for (let i = startMsgIndex; i < convStartIndices[convIndex + 1] 
-            || convIndex + 1 == convStartIndices.length; i++) {
-        
-        if (i >= messages.length) {
-            break
-        }
-        let msg = messages[i]
-        let uname = msg.author
-        let msgMillis  = msg.timestamp
-        
-        let userResult = result.find(elem => elem.name == uname)
-        if (userResult == undefined) {
-            continue
-        }
-        if (convIndex + 1 > userResult.responses + userResult.started + userResult.notResponded) {
-            if (uname == startUserName) {
-                userResult.started += 1
+            if (i >= messages.length) {
+                break
+            }
+            let msg = messages[i]
+            let uname = msg.author
+            let msgMillis  = msg.timestamp
+            
+            let userResult = result.find(elem => elem.name == uname)
+            if (userResult == undefined) {
                 continue
             }
-            userResult.responses += 1
-            userResult.responseTimes += msgMillis - convStartMillis
-        }    
-    }
-    result.forEach(userResult => {
-        if (convIndex + 1 > userResult.responses + userResult.started + userResult.notResponded) {
-            userResult.notResponded += 1
+            if (convIndex + 1 > userResult.responses + userResult.started + userResult.notResponded) {
+                if (uname == startUserName) {
+                    userResult.started += 1
+                    continue
+                }
+                userResult.responses += 1
+                userResult.responseTimes += msgMillis - convStartMillis
+            }    
         }
+        result.forEach(userResult => {
+            if (convIndex + 1 > userResult.responses + userResult.started + userResult.notResponded) {
+                userResult.notResponded += 1
+            }
+        })
     })
-})
 
-console.log(result)
+    return result
+}
